6c6607a730dcd11be3f86e93acc45ae467ad238f
serkanhancioglu <serkan@weteknoloji.com>
2026-02-26 02:26:03 +0300

    Lower Python requirement to 3.6 for server compatibility

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
     packages=find_packages(),
     install_requires=[
         "Flask>=2.0.0",
-        "flask-apispec>=0.12.0",
-        "apispec>=6.0.0",
+        "flask-apispec>=0.11.0",
+        "apispec>=5.0.0",
         "swagger-ui-bundle>=0.0.9",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
 )
