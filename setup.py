a0879f7d9bc62ff6e8b4379bcf747c1f51a5fc33
serkanhancioglu <serkan@weteknoloji.com>
2026-02-26 02:26:26 +0300

    Fix flask-apispec version to match server (0.11.x)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "Flask>=2.0.0",
-        "flask-apispec>=0.11.0",
+        "flask-apispec>=0.11.0,<0.12.0",
         "apispec>=5.0.0",
         "swagger-ui-bundle>=0.0.9",
     ],
